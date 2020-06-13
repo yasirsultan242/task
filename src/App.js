e49396e3d5b1a183440ac72905e3a6f27f48cbc2
yasirsultan242 <47809673+yasirsultan242@users.noreply.github.com>
2020-06-13 00:19:51 -0700

    practise

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React from 'react';
+
+import './App.css';
+// import Hook from './Comp/Hook';
+// import HookCounterThree from './Comp/HookCounterThree';
+// import HookCounterFour from './Comp/HookCounterFour';
+// import HookCounterFive from './Comp/HookCounterFive';
+// import HookMouse from './Comp/HookMouse';
+// import HookIntervalCounter from './Comp/HookIntervalCounter';
+// import DataFetching from './Comp/DataFetching';
+// import ComponentC from './Comp/ComponentC';
+
+// import Reducer from './Comp/Reducer';
+// import DataFetchingOne from './Comp/DataFetchingOne';
+// import Counter from './Comp/Counter';
+// import HookCounter from './Comp/HookCounter';
+import DataFetchingOne from './Comp/DataFetchingOne';
+export const UserContext =React.createContext()
+export const ChannelContext=React.createContext()
+
+
+
+function App() {
+  return (
+    <div className="App">
+
+<DataFetchingOne />
+
+{/* 
+<HookCounter /> */}
+{/* <Counter /> */}
+{/* <DataFetchingOne /> */}
+{/* <Reducer /> */}
+
+
+ {/* <UserContext.Provider value={'sultan'}>
+   <ChannelContext.Provider value={'helloS'}>
+   <ComponentC />
+   </ChannelContext.Provider>
+  
+   
+   </UserContext.Provider>   */}
+
+   </div>
+     );
+
+    }
+// <DataFetching />
+
+// {/* <HookIntervalCounter /> */}
+// {/* <HookMouse/ /> */}
+//    {/* <HookCounterFive /> */}
+// {/* <HookCounterThree /> */}
+// {/* <HookCounterFour /> */}
+
+
+//    {/* <Hook /> */}
+ 
+
+
+
+export default App;
